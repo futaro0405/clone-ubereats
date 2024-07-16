2e9ce4a4d69b907b861add5965734499baac1870
futaro0405 <fkswtkm0405.web@gmail.com>
2024-07-16 12:18:57 +0900

    add: フロントエンド環境構築

---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+
+export const Foods = ({match}) => {
+  return (
+    <Fragment>
+      フード一覧
+      <p>
+      restaurantsIdは {match.params.restaurantsId} です
+      </p>
+    </Fragment>
+  )
+}
